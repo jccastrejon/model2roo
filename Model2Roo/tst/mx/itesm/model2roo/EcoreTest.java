42c8af506ec9b53c7e762bfc146fe7500ca9ee99
jccastrejon <jccastrejon@e8f15543-8dba-432c-9eb4-6f5a14df147f>
2010-12-27 18:58:35 +0000

    Added license info
    git-svn-id: https://model2roo.googlecode.com/svn/trunk@36 e8f15543-8dba-432c-9eb4-6f5a14df147f

--- Model2Roo/tst/mx/itesm/model2roo/EcoreTest.java
+++ Model2Roo/tst/mx/itesm/model2roo/EcoreTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2010 jccastrejon, rosatzimba
+ *  
+ * This file is part of Model2Roo.
+ *
+ * Model2Roo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Model2Roo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Model2Roo.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package mx.itesm.model2roo;
 
 import static org.junit.Assert.assertEquals;
