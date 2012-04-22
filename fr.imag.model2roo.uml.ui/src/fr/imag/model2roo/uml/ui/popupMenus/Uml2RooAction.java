8f789da8b7d157223ece68571b0acd9ab0fc76a6
jccastrejon <jccastrejon@e8f15543-8dba-432c-9eb4-6f5a14df147f>
2012-04-22 21:00:31 +0000

    Added license info
    git-svn-id: https://model2roo.googlecode.com/svn/trunk@96 e8f15543-8dba-432c-9eb4-6f5a14df147f

--- fr.imag.model2roo.uml.ui/src/fr/imag/model2roo/uml/ui/popupMenus/Uml2RooAction.java
+++ fr.imag.model2roo.uml.ui/src/fr/imag/model2roo/uml/ui/popupMenus/Uml2RooAction.java
@@ -1,13 +1,21 @@
-/*******************************************************************************
- * Copyright (c) 2008, 2011 Obeo.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     Obeo - initial API and implementation
- *******************************************************************************/
+/*
+ * Copyright 2012 jccastrejon
+ *  
+ * This file is part of Model2Roo.
+ *
+ * Model2Roo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Model2Roo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Model2Roo.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.imag.model2roo.uml.ui.popupMenus;
 
 import java.io.BufferedReader;
