3de94a431ff61d84892ed480c4a4429ccb170d25
Juan Castrejón <jccastrejon@gmail.com>
2012-11-28 10:30:17 +0100

    Fixed createRest mapping

--- fr.imag.model2roo.addon.polyglot/src/main/resources/fr/imag/model2roo/addon/polyglot/restMethods-template.java
+++ fr.imag.model2roo.addon.polyglot/src/main/resources/fr/imag/model2roo/addon/polyglot/restMethods-template.java
@@ -1,5 +1,6 @@
 __INIT_IMPORTS__
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,10 +8,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 __END_IMPORTS__
 
 __INIT_METHODS__
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value="/createRest", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public void createRest(@RequestBody __ENTITY__ __ENTITY_LOWER__) {
         __ENTITY_LOWER__Repository.save(__ENTITY_LOWER__);
+    }
+
+    @RequestMapping(value="/list", method = RequestMethod.GET)
+    public void listRest(Model uiModel) {
+        uiModel.addAttribute("list", __ENTITY_LOWER__Repository.findAll());
     }
 __END_METHODS__
